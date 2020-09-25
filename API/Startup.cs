ebca2a70ade9f7c5ff1b791182371cc90bf593ec
santiagosoto <santiago_Soto86@yahoo.com.ar>
2020-09-25 20:46:39 -0300

    Conexion entre el cliente y el servidor

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                }));
             services.AddControllers();
         }
 
@@ -36,10 +41,12 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
