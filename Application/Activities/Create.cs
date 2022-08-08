394baf060b4e62f725fc7af0de3ead9636b465fc
Santiago Soto <santiagorsoto@gmail.com>
2022-08-07 23:22:35 -0300

    Fin de seccion 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,11 @@
-﻿using Application.Core;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -12,6 +16,7 @@ namespace Application.Activities
         {
             public Activity Activity { get; set; }
         }
+
         public class CommandValidator : AbstractValidator<Command>
         {
             public CommandValidator()
@@ -19,25 +24,39 @@ namespace Application.Activities
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
         }
+
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
-
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
+
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
-               _context.Activities.Add(request.Activity);
+                var user = await _context.Users.FirstOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetUsername());
 
-               var result = await _context.SaveChangesAsync() > 0;
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
 
-               if (!result) return Result<Unit>.Failture("Failed to create activity");
+                request.Activity.Attendees.Add(attendee);
 
-               return Result<Unit>.Success(Unit.Value);
+                _context.Activities.Add(request.Activity);
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failture("Failed to create activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
-
     }
 }
